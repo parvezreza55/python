45fdc715f7ec9cfa2b94a265c6c16761671b7468
parvez <parvezreza112@gmail.com>
2026-01-29 20:12:05 +0600

    added a file

---
+++ logical.py
@@ -0,0 +1,9 @@
+num1=20
+num2=50
+num3=30
+if num1>num2 and num1>num3:
+    print("The largest number is :",num1)
+if num2>num1 and num2>num3:
+    print("The largest number is :",num2)
+else:
+    print("The largest number is :",num3)
