f81d6e5b12c3605376c40c07b3bdf548091138b4
parvez <parvezreza112@gmail.com>
2026-01-31 11:55:56 +0600

    added new file

---
+++ latter_grade_logical.py
@@ -0,0 +1,7 @@
+marks= 79
+if marks>=80 and marks<=100:
+    print("The letter grade A+")
+if marks>=70 and marks<=79:
+    print("The letter grade A")
+if marks>=60 and marks<=69:
+    print("The letter grade A-")
