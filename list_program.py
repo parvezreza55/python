1735206ff43d41b2e9935ee2adcac4e48f234e1f
parvez <parvezreza112@gmail.com>
2026-02-08 19:17:34 +0600

    added listing basic

--- list_program.py
+++ list_program.py
@@ -1,6 +1,6 @@
 # lists:
 
-subject=['apple','banana','mango','lime','pineapple']
+subject=['apple','banana','mango','lime']
 print(subject)
 print(subject[:-2])
 print(subject[1:4])
@@ -18,5 +18,30 @@ print(subject * 3)
 
 # length of list
 print(len(subject))
+
+# to add one element one the last
 subject.append("pineapple")
 print(subject)
+
+# To add manually on the specific index use insert(index_no,"elements")
+subject.insert(2,"orange")
+print(subject)
+
+# To remove any elements use remove("name")
+subject.remove("pineapple")
+print(subject)
+
+# TO sort any list use .sort()
+subject.sort()
+print(subject)
+
+#copy index and count
+pos=subject.index("apple")
+print(pos)
+
+subject2=subject.copy()
+print(subject2)
+
+counts =subject.count("apple")
+print(counts)
+
--- string_check.py
+++ string_check.py
@@ -5,6 +5,10 @@ print(a[2])
 print(a[3:6])
 print(a[::2])
 print(a.split(","))
-
+age = 22
 #merge string
 print(b + " "+ c )
+print(f"{age} years old")
+
+txt = "Hello Wor\rld!"
+print(txt)
