11d6b62eeb46af1e90ae35d0ea21d13337e08c6a
parvez <parvezreza112@gmail.com>
2026-02-10 02:08:25 +0600

    added pattern

---
+++ patern.py
@@ -0,0 +1,10 @@
+n=4
+for i in range(n+1):
+    print((n-i) * " " + i*'*')
+for i in range(n+1):
+    print((n-i) * " " + (2*i-1) *'*')
+for i in range(n+1):
+    print(i*"*")
+
+for i in range(n+1):
+    print((2*i-1)*"*")
